e0b9c20fda15a703837a0e4e08d64da619294ea3
Kellen Piffner <k@8labs.com>
2018-10-01 14:57:06 -0700

    starter process blog posts.

--- scripts/tags.js
+++ scripts/tags.js
@@ -1,11 +1,21 @@
 ﻿var tag = hexo.extend.tag;
 
 tag.register('limg', function (args, content) {
-    // "local" img helper. glues the root to the passed path
-    // the rest is glued on as img tag attrs.
+  // "local" img helper. glues the root to the passed path
+  // the rest is glued on as img tag attrs.
 
-    var src = args.shift();  // always have to specify src
-    var attrs = args.join(' '); // everything else is 'as is'
+  var src = args.shift();  // always have to specify src
 
-    return '<img src="' + hexo.config.root + src + '" ' + attrs + '>';
+  // quotes get stripped off the args
+  // let's just hack them back in for attributes
+  for (let i in args) {
+    if (args[i].includes("=")) {
+      args[i] = args[i].replace('=', '="') + '"';
+    }
+    console.log(args[i], args);
+  }
+
+  var attrs = args.join(' ');
+
+  return '<img src="' + hexo.config.root + src + '" ' + attrs + '>';
 });
